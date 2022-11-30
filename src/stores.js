513cab482452b053709fe99af767ab02fce1d63d
Ryan Marx <ryanbmarx@gmail.com>
2022-11-30 11:44:54 -0600

    New paradigm for data

--- src/stores.js
+++ src/stores.js
@@ -1,7 +1,7 @@
 import { writable } from "svelte/store";
 
-export const yearly = writable([]);
-export const cumulative = writable([]);
+export const target = writable([]);
+export const baseline = writable([]);
 export const multipliers = writable({
 	scope: "scope1",
 	growth: "growth_sp",
