eff4f5591c5da1b0089bc3336ef83635ab937111
Ryan Marx <ryanbmarx@gmail.com>
2022-12-07 22:07:28 -0600

    More styled and accesible

--- src/components/PickerSector.svelte
+++ src/components/PickerSector.svelte
@@ -1,18 +1,67 @@
 <script>
 	export let sectors = {};
-	import { fly } from "svelte/transition";
 	export let sectorHeader;
 	export let value;
 
+	let button; // The main menu button
 	let showMenu = false;
 
 	const isMobile = false;
+	const menuItems = [];
 
 	function open() {
 		showMenu = true;
 	}
 	function close() {
 		showMenu = false;
+		button.focus();
+	}
+	function registerMenuItem(node) {
+		menuItems.push(node);
+	}
+
+	function onKeydown(e) {
+		const { target, key } = e;
+		const targetIndex = menuItems.indexOf(target);
+		switch (key) {
+			case "Space":
+			case "Enter":
+				if (target === button) {
+					showMenu ? close() : open();
+				} else {
+					// this is a menu item
+					const { sector } = target.dataset;
+					value = sector;
+					console.log("New sector", sector, target);
+					close();
+				}
+				break;
+			case "Escape":
+				console.log("Closing, escape key");
+				close();
+				break;
+			case "Down": // IE/Edge specific value
+			case "ArrowDown":
+				if (target === button) {
+					menuItems[0].focus();
+				} else {
+					let nextIndex = targetIndex + 1;
+					if (nextIndex >= menuItems.length) nextIndex = currentIndex;
+					menuItems[nextIndex].focus();
+				}
+				break;
+			case "Up": // IE/Edge specific value
+			case "ArrowUp":
+				// Skip this if it's the top button
+				if (target === button) return;
+
+				// Move up to the main button from the top menu item
+				if (targetIndex === 0) button.focus;
+
+				// Move back a menu item
+				menuItems[targetIndex - 1].focus();
+				break;
+		}
 	}
 </script>
 
@@ -55,8 +104,7 @@
 		border: none;
 		position: absolute;
 		left: 0;
-		padding-bottom: 0.25em;
-		padding-left: 3px;
+		padding: 3px 1.5rem 0.25em 0;
 		border-bottom: 3px solid var(--color-accent);
 		cursor: pointer;
 	}
@@ -65,10 +113,24 @@
 		display: inline-block;
 		width: 0.5rem;
 		height: 0.5rem;
-		margin: 0 0 0 0.75rem;
+
+		position: absolute;
+		top: 50%;
+		right: 0;
+
 		border-left: 2px solid currentColor;
 		border-bottom: 2px solid currentColor;
-		transform: rotate(-45deg) translate(0, -50%);
+		transform: translate(0, -50%) rotate(45deg);
+		transform-origin: center;
+		transition: transform var(--speed-transition);
+	}
+	.picker__label:hover,
+	.picker__label:focus {
+		outline: none;
+	}
+	.open.picker__label::after,
+	.open.picker__label::after {
+		transform: translate(0, -50%) rotate(-45deg);
 	}
 
 	.menu {
@@ -77,38 +139,71 @@
 		left: 0;
 		padding-top: var(--gap);
 		z-index: 10;
+		transition: all var(--speed-transition) ease-in-out;
+		opacity: 0;
+		transform: translate(0, -15px);
+	}
+
+	.menu.visible {
+		opacity: 1;
+		transform: translate(0);
+	}
+	.menu__select {
+		list-style: none;
+		margin: 0;
+		padding: 0;
 	}
 	.menu__btn {
 		display: block;
 		width: 100%;
 		text-align: left;
 		font: bold var(--font-size) / 1em var(--sans-serif-fonts);
-		padding: 0.5rem;
-		border: none;
-		background-color: white;
+		padding: 0.5rem 0.5rem 0.5rem 1rem;
+		background-color: var(--color-accent);
+		color: var(--color-accent-text);
+		box-shadow: 3px 3px 6px rgba(0, 0, 0, 0.4);
+		border-bottom: 3px solid rgba(255, 255, 255, 0.8);
 		cursor: pointer;
+		position: relative;
 		transition: background-color var(--speed-transition) ease-in-out,
 			color var(--speed-transition) ease-in-out;
 	}
 
-	.menu__select {
-		box-shadow: 3px 3px 6px rgba(0, 0, 0, 0.4);
-		background-color: white;
-		list-style: none;
-		margin: 0;
-		padding: 0;
+	.menu__btn:last-child {
+		border-bottom: none;
 	}
 
-	.menu__btn:hover {
-		background-color: var(--color-gray-light);
+	.menu__btn::before {
+		content: "";
+		display: block;
+		height: 0.35rem;
+		width: 0.35rem;
+		background-color: transparent;
+		border-radius: 50%;
+
+		position: absolute;
+		top: 50%;
+		left: 0.5rem;
+		transform: translate(-50%, -50%);
+		transition: background-color var(--speed-transition);
 	}
+
+	.menu__btn:first-child {
+		border-radius: 0.5rem 0.5rem 0 0;
+	}
+	.menu__btn:last-child {
+		border-radius: 0 0 0.5rem 0.5rem;
+	}
+
+	.menu__btn:hover,
 	.menu__btn:focus {
-		outline: 2px solid var(--color-accent);
+		background-color: #28597a;
+		border-color: rgba(255, 255, 255, 0.5);
+		outline: none;
 	}
-
-	.menu__btn.active {
-		background-color: var(--color-accent);
-		color: var(--color-accent-text);
+	.menu__btn:hover::before,
+	.menu__btn:focus::before {
+		background-color: currentColor;
 	}
 
 	.not-mobile {
@@ -147,14 +242,14 @@
 <div class="picker not-mobile" arial-label="About the {value} sector">
 	<span class="header">About</span>
 	<button
-		on:keydown={e => {
-			console.log(e);
-			if (e.key.toLowerCase() === "escape") close();
-		}}
+		bind:this={button}
+		on:keydown|preventDefault={onKeydown}
 		on:mouseover={open}
 		on:mouseleave={close}
 		on:focus={open}
+		on:focusin={open}
 		on:blur={close}
+		on:focusout={close}
 		on:click|self={e => {
 			if (showMenu) {
 				close();
@@ -162,25 +257,32 @@
 				open();
 			}
 		}}
-		class="picker__label">
+		aria-controls="sector-menu"
+		aria-expanded={showMenu}
+		class="picker__label"
+		class:open={showMenu}>
 		{sectorHeader}
-		{#if showMenu}
-			<div class="menu" transition:fly={{ y: -25, duration: 250 }}>
-				<menu class="menu__select">
-					{#each Object.entries(sectors) as [id, { heading }]}
-						<li>
-							<button
-								class="menu__btn"
-								class:active={id === value}
-								on:click={e => {
-									value = id;
-									close();
-								}}>
-								{heading}</button>
-						</li>
-					{/each}
-				</menu>
-			</div>
-		{/if}
+
+		<div class="menu" class:visible={showMenu}>
+			<menu id="sector-menu" class="menu__select">
+				{#each Object.entries(sectors) as [id, { heading }]}
+					<!-- aria-activedescendant:  Set to the ID of the focused item, if there is one.-->
+					<li
+						data-sector={id}
+						id="sector-menu-{id.toLowerCase()}"
+						role="button"
+						class="menu__btn"
+						class:active={id === value}
+						on:click={e => {
+							value = id;
+							close();
+						}}
+						on:keydown|preventDefault={onKeydown}
+						use:registerMenuItem>
+						{heading}
+					</li>
+				{/each}
+			</menu>
+		</div>
 	</button>
 </div>
