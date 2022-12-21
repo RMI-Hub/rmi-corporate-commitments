73801461fe70318bb681aeddef93ade00ef4fa49
Ryan Marx <ryanbmarx@gmail.com>
2022-12-21 15:30:47 -0600

    Make place for presets data to live

--- src/main.js
+++ src/main.js
@@ -1,6 +1,7 @@
 import App from "./App.svelte";
 import microcopy from "./config/microcopy.json";
 import toggles from "./config/toggles.json";
+import presets from "./config/presets.json";
 import sectors from "./config/sectors.json";
 
 const app = new App({
@@ -8,6 +9,7 @@ const app = new App({
 	target: document.getElementById("rmi"),
 	props: {
 		...microcopy,
+		presets,
 		toggles,
 		sectors,
 	},
