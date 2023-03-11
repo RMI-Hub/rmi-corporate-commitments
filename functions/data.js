721fbbfe8748dcfc66d5662eed5c548dc23ed05b
Ryan Marx <ryanbmarx@gmail.com>
2023-03-11 13:04:09 -0600

    lint and order

--- functions/data.js
+++ functions/data.js
@@ -8,29 +8,6 @@ const path = require("path");
 // THis is our data cache
 const sectors = new Map();
 
-// /**
-//  *
-//  * Takes our two data sets and convers them into a stack, so we can get the overall domain and set each chart to the same scale
-//  * @param {*} target
-//  * @param {*} baseline
-//  * @param {*} companies
-//  * @returns
-//  */
-// async function getYearlyMax(target, baseline, companies) {
-// 	const { stack, max } = await import("d3");
-// 	let maxValue = 0;
-// 	const stacker = stack().keys(companies);
-// 	for (let d of [target.yearly, baseline.yearly]) {
-// 		const s_data = stacker(d);
-// 		maxValue = Math.max(
-// 			maxValue,
-// 			max(s_data[s_data.length - 1], d => d[1])
-// 		);
-// 	}
-// 	// Nudge the max value a little higher so the scale definitely encapsulates it
-// 	return maxValue * 1.1;
-// }
-
 /**
  * THIS IS THE MAIN THING FOR THE CLOUD FUNCTIONS. THIS PARENT FUNCTION WILL GRAB
  * RAW DATA AND CREATE SOMETHING CHARTABLE.
