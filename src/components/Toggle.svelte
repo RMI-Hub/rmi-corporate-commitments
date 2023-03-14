4aabc978bc4154f47bc50180d1e69b011ea5a8a7
Ryan Marx <ryanbmarx@gmail.com>
2023-03-14 12:27:37 -0500

    Nicer looking toggle

--- src/components/Toggle.svelte
+++ src/components/Toggle.svelte
@@ -40,14 +40,53 @@
 	}
 
 	.toggles__btn {
-		font-size: 1em;
-		accent-color: var(--color-accent);
+		clip-path: inset(50%);
+		height: 1px;
+		overflow: hidden;
+		position: absolute;
+		white-space: nowrap;
+		width: 1px;
 	}
 
 	.label {
 		font-size: var(--font-size-small);
 		flex-basis: 100%;
 		margin: 0;
+	}
+
+	.toggles__box {
+		flex: 0 0;
+		position: relative;
+		display: block;
+		min-width: 1.5em;
+		min-height: 1.5em;
+		max-width: 1.5em;
+		max-height: 1.5em;
+	}
+
+	.toggles__box::after,
+	.toggles__box::before {
+		display: block;
+		content: "";
+		position: absolute;
+		border-radius: 50%;
+	}
+	.toggles__box::before {
+		border: 2px solid var(--color-font);
+		inset: 0;
+	}
+
+	.toggles__box::after {
+		width: 50%;
+		height: 50%;
+		top: 50%;
+		left: 50%;
+		background: var(--color-accent);
+		transform: translate(-50%, -50%) scale(0);
+		transition: transform var(--speed-transition) ease-in-out;
+	}
+	input:checked + .toggles__box::after {
+		transform: translate(-50%, -50%) scale(1);
 	}
 </style>
 
@@ -59,7 +98,7 @@
 	{#each buttons as { text, value }}
 		<label class="toggles__label sans-serif" for={slugify(text)}>
 			<input
-				class="toggles__btn"
+				class="toggles__btn "
 				type="radio"
 				id={slugify(text)}
 				name={slugify(label)}
@@ -68,6 +107,7 @@
 				}}
 				{value}
 				bind:group={$multipliers[id]} />
+			<span class="toggles__box" />
 			{text}
 		</label>
 	{/each}
